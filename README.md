9071a11770a2a2c8bb988a896405b0b5f6a2577c
Kei Kamikawa <x00.x7f@gmail.com>
2019-01-24 10:39:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Lisp::Tiny - It's new $module
+
+# SYNOPSIS
+
+    use Lisp::Tiny;
+
+# DESCRIPTION
+
+Lisp::Tiny is ...
+
+# LICENSE
+
+Copyright (C) Kei Kamikawa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kei Kamikawa <x00.x7f@gmail.com>
---
+++ lib/Lisp/Tiny.pm
@@ -0,0 +1,39 @@
+package Lisp::Tiny;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Lisp::Tiny - It's new $module
+
+=head1 SYNOPSIS
+
+    use Lisp::Tiny;
+
+=head1 DESCRIPTION
+
+Lisp::Tiny is ...
+
+=head1 LICENSE
+
+Copyright (C) Kei Kamikawa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kei Kamikawa E<lt>x00.x7f@gmail.comE<gt>
+
+=cut
+
